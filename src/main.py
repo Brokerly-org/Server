d391ce886997a7fe26965873f430342af3337fc4
hvuhsg5@gmail.com <hvuhsg6@gmail.com>
2021-08-18 15:09:41 +0300

    remove debug flag

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from uvicorn import run
 
 
 def main():
-    run("api:app", host="0.0.0.0", port=9981, debug=True) # ssl_keyfile="..\https_cert\key.pem", ssl_certfile="..\https_cert\cert.pem")
+    run("api:app", host="0.0.0.0", port=9981) # ssl_keyfile="..\https_cert\key.pem", ssl_certfile="..\https_cert\cert.pem")
 
 if __name__ == "__main__":
     main()
