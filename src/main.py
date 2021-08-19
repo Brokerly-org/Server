f0c2a611a4eccd272545d253ca837315c908ded9
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2021-08-19 03:40:13 +0300

    env variables and gitignore

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from uvicorn import run
+from dotenv import load_dotenv
+from os import environ
 
+load_dotenv()
+PRODUCTION = environ.get('APP_ENV') == 'production'
+DEBUG = True if not PRODUCTION else False
 
 def main():
-    run("api:app", host="0.0.0.0", port=9981) # ssl_keyfile="..\https_cert\key.pem", ssl_certfile="..\https_cert\cert.pem")
+    run("api:app", host="0.0.0.0", port=9981, debug=DEBUG) # ssl_keyfile="..\https_cert\key.pem", ssl_certfile="..\https_cert\cert.pem")
 
 if __name__ == "__main__":
     main()
